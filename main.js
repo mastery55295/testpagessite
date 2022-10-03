c805d7e54665bb87a241b5f78db94d095447a6f0
mastery55295 <93741731+mastery55295@users.noreply.github.com>
2022-10-03 13:44:35 -0500

    send console.log to an html page

---
+++ main.js
@@ -0,0 +1,9 @@
+var realConsoleLog = console.log;
+    console.log = function () {
+        var message = [].join.call(arguments, " ");
+        // Display the message somewhere... (jQuery example)
+        $(".output").text(message);
+        realConsoleLog.apply(console, arguments);
+    };
+
+console.log("test");
